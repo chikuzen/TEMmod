ef1cacc4fb8689aa02772c82f78c428069cf9e56
Oka Motofumi <chikuzen.mo@gmail.com>
2013-08-11 01:49:25 +0900

    bump version to 0.2.0

--- src/temmod.h
+++ src/temmod.h
@@ -30,7 +30,7 @@
 #include <windows.h>
 #include "avisynth.h"
 
-#define TEMM_VERSION "0.1.1"
+#define TEMM_VERSION "0.2.0"
 
 
 typedef void (__stdcall *calc_map_func)(const uint8_t* srcp, uint8_t* dstp,
